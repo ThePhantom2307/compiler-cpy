1de7ba0ce56ef8473c600565f15caa3337a7564b
PanagiotisT2307 <trypos2307@gmail.com>
2024-03-06 15:51:47 +0200

    first version of lexical analyzer

--- compiler.py
+++ compiler.py
@@ -5,95 +5,129 @@ def lexicalAnalyzer():
     global code_line
     global code_index
     global code_file
+    global flag_eof
     
     token_type = "NONE"
 
     current_char = ""
     next_char = code_file.read(1)
+    current_code_index = code_index
     
     ## Checking if the file is empty
-    while (next_char != ""):
+    if (next_char == ""):
+        flag_eof = True
+        return (token_type, 0, code_line, current_code_index)        
         
-        ## Checking for white spaces
-        while (next_char == " " or next_char == "\n" or next_char == "\t"):
-            if (next_char == "\n"):
-                code_line += 1
-                code_index = 0
+    ## Checking for white spaces
+    while (next_char == " " or next_char == "\n" or next_char == "\t"):
+        if (next_char == "\n"):
+            code_line += 1
+            code_index = 0
+        next_char = code_file.read(1)
+        code_index += 1
+            
+    ## Checking if there is identifier or keyword       
+    if (next_char.isalpha()):
+        current_char = next_char
+        next_char = code_file.read(1)
+        code_index += 1
+            
+        while (next_char.isalpha() or next_char.isdigit()):
+            current_char += next_char
             next_char = code_file.read(1)
             code_index += 1
-            
-        ## Checking if there is identifier or keyword       
-        if (next_char.isalpha()):
-            current_char = next_char
-            next_char = code_file.read(1)
-            code_index += 1
-            
-            while (next_char.isalpha() or next_char.isdigit()):
-                current_char += next_char
-                next_char = code_file.read(1)
-                code_index += 1
              
-            if (current_char in keywords):
-                token_type = "KEYWORD"
-                return (token_type, current_char, code_line, code_index)
-            else:
-                token_type = "IDENTIFIER"
-                return (token_type, current_char, code_line, code_index)
+        if (current_char in keywords):
+            token_type = "KEYWORD"
+            return (token_type, current_char, code_line, current_code_index)
+        else:
+            token_type = "IDENTIFIER"
+            return (token_type, current_char, code_line, current_code_index)
         
-        ## Checking for number
-        if (next_char.isdigit()):
-            current_char = next_char
+    ## Checking for number
+    elif (next_char.isdigit()):
+        current_char = next_char
+        next_char = code_file.read(1)
+        code_index += 1
+
+        while (next_char.isdigit()):
+            current_char += next_char
             next_char = code_file.read(1)
             code_index += 1
 
-            while (next_char.isdigit()):
-                current_char += next_char
-                next_char = code_file.read(1)
-                code_index += 1
+        if (next_char.isalpha()):
+            print("Error in line " + str(code_line) + " and index " + str(current_code_index) + ": Letter \"" + next_char + "\" found after digits \"" + current_char)
+        else:
+            token_type = "NUMBER"
+            return (token_type, current_char, code_line, current_code_index)
 
-            if (next_char.isalpha()):
-                print("Error in line " + str(code_line) + " and index " + str(code_index) + ": Letter \"" + next_char + "\" found after digits \"" + current_char)
-            else:
-                token_type = "NUMBER"
-                return (token_type, current_char, code_line, code_index)
+    elif (next_char == "#"):
+        current_char = next_char
+        next_char = code_file.read(1)
+        code_index += 1
 
         if (next_char == "#"):
             current_char = next_char
             next_char = code_file.read(1)
             code_index += 1
-
-            if (next_char == "#"):
+                
+            while (next_char != current_char and current_char == "#"):
                 current_char = next_char
                 next_char = code_file.read(1)
                 code_index += 1
-                while (next_char != current_char and current_char == "#"):
-                    current_char = next_char
-                    next_char = code_file.read(1)
-                    code_index += 1
-            else:
-                while (next_char.isalpha()):
-                    current_char += next_char
-                    next_char = code_file.read(1)
-                    code_index += 1
+                
+                if (next_char == "\n"):
+                    code_line += 1
+ 
+        elif (next_char.isalpha()):
+                
+            while (next_char.isalpha()):
+                current_char += next_char
+                next_char = code_file.read(1)
+                code_index += 1
             
-                if (current_char in keywords):
-                    token_type = "KEYWORD"
-                    return (token_type, current_char, code_line, code_index)
-                else:
-                    print("Error in line " + str(code_line) + " and index " + str(code_index) + ": not recognizing the value \"" + current_char+ "\"")
+            if (current_char in keywords):
+                token_type = "KEYWORD"
+                return (token_type, current_char, code_line, current_code_index)
+            else:
+                print("Error in line " + str(code_line) + " and index " + str(current_code_index) + ": Not recognizing the value \"" + current_char + "\"")
+        else:
+            current_char = next_char
+            next_char = code_file.read(1)
+            code_index += 1
                 
-                
+            if (next_char == "{"):
+                current_char += next_char
+                code_index += 1
+                token_type = "GROUP"
+                return (token_type, current_char, code_line, current_code_index)
+            elif (next_char == "}"):
+                current_char += next_char
+                code_index += 1
+                token_type = "GROUP"
+                return (token_type, current_char, code_line, current_code_index)
+    elif (next_char == "(" or next_char == ")"):
+        current_char = next_char
+        code_index += 1
+        token_type = "GROUP"
+        return (token_type, current_char, code_line, current_code_index)
+    else:
+        print("Error in line " + str(code_line) + " and index " + str(current_code_index) + ": Not recognizing the value \"" + current_char + "\"")
+
     ## The format of the return set is (token_type, value, line, index)
     return (token_type, 0, 0, 0)
 
 ### The main function for all processes
 def startProcessing():
-    lexicalAnalyzer()
+    while (flag_eof == False):
+        lexicalAnalyzer()
     
 ### Main Program
 if __name__ == "__main__":
-    code_line = 0
-    code_index = 0
+    print("Start compiling")
+    code_line = 1
+    code_index = 1
+    flag_eof = False
     
     keywords = ("main", "def", "#def", "#int", "global", "if",
                 "elif", "else", "while", "print", "return",
@@ -101,3 +135,4 @@ if __name__ == "__main__":
 
     code_file = open(sys.argv[1], "r")
     startProcessing()
+    print("Finish compiling")
